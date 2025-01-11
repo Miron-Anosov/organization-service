c52d63c9ebc8b95530c01d35b1b3188fc87fa24c
Miron-Anosov <mairon26rus@gmail.com>
2025-01-11 18:13:15 +0100

    feat!: create static header: X-API-KEY

---
+++ src/core/api/v1/dependencies/common/api_key.py
@@ -0,0 +1,31 @@
+"""Static API key."""
+
+from fastapi import HTTPException, Security
+from fastapi.security import APIKeyHeader
+from starlette.status import HTTP_403_FORBIDDEN
+
+from src.core.api.v1.presentation.responses.error import ErrResponse
+
+API_KEY = "your_api_key_here"  # TODO: вынести в .env
+api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
+
+
+async def get_api_key(
+    api_key_header_req: str = Security(api_key_header),
+) -> None:
+    """Check header of X-API-Key.
+
+    :param api_key_header_req:
+    :type api_key_header_req: str
+    :return: None
+    :rtype: type[None]
+    :raises HTTPException: Could not validate API key
+    """
+    if api_key_header_req != API_KEY:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN,
+            detail=ErrResponse(
+                error_type="AuthenticationError",
+                error_message="X-API-Key header is incorrect.",
+            ).model_dump(),
+        )
